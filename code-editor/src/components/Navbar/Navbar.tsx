a9f91df9b00035cf0debde2fcfc6aca6f470ee9a
Bogdan Istrate <bogdanistrate03@gmail.com>
2024-11-15 14:22:17 +0200

    Add rest of files

---
+++ code-editor/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-4 px-6 md:px-12 lg:px-24 bg-gray-900/90 backdrop-blur-md shadow-lg ">
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logonobg.png"
+          alt="CodeEditor"
+          width={50}
+          height={50}
+          className="h-auto w-auto"
+        />
+        <span className="ml-2 text-2xl font-semibold text-white hidden md:inline-block">
+          Code Editor
+        </span>
+      </Link>
+
+      <div className="flex items-center space-x-6">
+        <Link
+          href="/docs"
+          className="text-gray-300 hover:text-white text-sm font-medium hidden sm:block transition-all duration-150"
+        >
+          Docs
+        </Link>
+        <button
+          className="flex items-center space-x-2 px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md 
+                     hover:bg-blue-500 focus:ring-2 focus:ring-blue-300 focus:outline-none 
+                     transition-all duration-200 ease-in-out"
+        >
+          <span>Sign In</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
